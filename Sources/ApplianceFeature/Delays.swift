6fcb68c6ed09023d41a5c2907cb81b741a76c8d1
Renaud Jenny <renaud.jenny@gmail.com>
2023-07-19 22:17:28 +0200

    feat(Appliance Feature): implement off peak periods in Delays

--- Sources/ApplianceFeature/Delays.swift
+++ Sources/ApplianceFeature/Delays.swift
@@ -1,32 +1,79 @@
 import ComposableArchitecture
+import Foundation
+import Models
 
 public struct Delays: Reducer {
     public struct State: Equatable {
         var program: Program
         var appliance: Appliance
+        var items: [Item]
+
+        public init(program: Program, appliance: Appliance) {
+            self.program = program
+            self.appliance = appliance
+            self.items = ([Delay(hour: 0, minute: 0)] + appliance.delays).map {
+                Item(delay: $0, minutesOffPeak: 0, minutesInPeak: 0)
+            }
+        }
     }
-    public enum Action: Equatable {}
+    public enum Action: Equatable {
+        case task
+    }
+
+    @Dependency(\.date) var date
+    @Dependency(\.calendar) var calendar
+    @Dependency(\.periodProvider) var periodProvider
+
     public var body: some ReducerOf<Self> {
-        EmptyReducer()
+        Reduce { state, action in
+            switch action {
+            case .task:
+                let offPeackPeriods = offPeakPeriods
+                state.items = state.items.map {
+                    var item = $0
+                    // TODO: find minutes In/Off peak rules and apply that
+                    item.minutesInPeak = 0
+                    item.minutesOffPeak = 0
+                    return item
+                }
+                return .none
+            }
+        }
+    }
+
+    private var offPeakPeriods: [(start: Date, end: Date)] {
+        periodProvider.get().flatMap { period in
+            var start = period.start
+            start.year = calendar.component(.year, from: date())
+            start.month = calendar.component(.month, from: date())
+            start.day = calendar.component(.day, from: date())
+            var end = period.end
+            end.year = calendar.component(.year, from: date())
+            end.month = calendar.component(.month, from: date())
+            end.day = calendar.component(.day, from: date())
+
+            return (-1...1).compactMap { day -> (start: Date, end: Date)? in
+                let day = TimeInterval(day)
+                guard let offPeakStartDate = calendar.date(from: start)?.addingTimeInterval(day * 60 * 60 * 24),
+                      let offPeakEndDate = calendar.date(from: end)?.addingTimeInterval(day * 60 * 60 * 24)
+                else { return nil }
+                if offPeakEndDate > offPeakStartDate {
+                    return (start: offPeakStartDate, end: offPeakEndDate)
+                } else {
+                    let offPeakEndDate = offPeakEndDate.addingTimeInterval(60 * 60 * 24)
+                    return (start: offPeakStartDate, end: offPeakEndDate)
+                }
+            }
+        }
     }
 }
 
 extension Delays.State {
     struct Item: Identifiable, Equatable {
         let delay: Delay
-        let minutesOffPeak: Int
-        let minutesInPeak: Int
+        var minutesOffPeak: Int
+        var minutesInPeak: Int
 
         var id: Delay.ID { delay.id }
-    }
-
-    var items: [Item] {
-        ([Delay(hour: 0, minute: 0)] + appliance.delays).map {
-            Item(
-                delay: $0,
-                minutesOffPeak: Int(program.duration/60 * 20/100),
-                minutesInPeak: Int(program.duration/60 * 80/100)
-            )
-        }
     }
 }
