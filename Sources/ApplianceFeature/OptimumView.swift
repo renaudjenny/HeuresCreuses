80b62bb213aaf1f3fed98d9a9ac839178cc45927
Renaud Jenny <renaud.jenny@gmail.com>
2023-07-29 16:47:11 +0200

    feat(Appliance Feature): improve Optimum View
    * Add fake data for now to demonstrate the idea

--- Sources/ApplianceFeature/OptimumView.swift
+++ Sources/ApplianceFeature/OptimumView.swift
@@ -16,9 +16,19 @@ struct OptimumView: View {
     var body: some View {
         WithViewStore(store, observe: ViewState.init) { viewState in
             VStack {
-                Text("Optimum").font(.title2).padding()
+                Text("Optimum").font(.title).padding()
 
-                Text("TODO")
+                Text("The **optimum** is the best time to start your appliance from now with the indicated delay")
+                    .font(.subheadline)
+                    .padding([.horizontal, .bottom])
+
+                Text("Wait **23 minutes** before starting your appliance with the **2 hours delay** to be **100%** off peak")
+                    .padding(.horizontal)
+
+                Button { } label: {
+                    Label("Send me a notification in 23 minutes", systemImage: "bell.badge")
+                }
+                .padding()
 
                 Spacer()
                 Button { viewState.send(.delaysTapped(viewState.program)) } label: {
@@ -26,7 +36,21 @@ struct OptimumView: View {
                 }
             }
             .padding()
-            .presentationDetents([.fraction(1/4)])
+            .presentationDetents([.medium])
         }
     }
 }
+
+#if DEBUG
+struct OptimumView_Preview: PreviewProvider {
+    static var previews: some View {
+        Color.blue
+            .sheet(isPresented: .constant(true)) {
+                OptimumView(store: Store(
+                    initialState: Optimum.State(program: Appliance.dishwasher.programs.first!, appliance: .dishwasher),
+                    reducer: Optimum()
+                ))
+            }
+    }
+}
+#endif
