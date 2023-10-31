b96bbbdf001ca19d748b62a973ab035efc072037
Renaud Jenny <renaud.jenny@gmail.com>
2023-10-31 17:51:25 +0100

    feat(OffPeak): improve Off Peak Selection View

--- Sources/OffPeak/OffPeakSelectionView.swift
+++ Sources/OffPeak/OffPeakSelectionView.swift
@@ -32,15 +32,48 @@ public struct OffPeakSelectionView: View {
         WithViewStore(store, observe: ViewState.init) { viewState in
             Form {
                 ForEach(viewState.periods) { period in
-                    HStack {
-                        Text(period.start.description)
-                        Image(systemName: "arrowshape.forward")
-                        Text(period.end.description)
-                    }
+                    period.clockView
                 }
             }
             .navigationTitle("Off peak periods")
         }
+    }
+}
+
+private extension DateComponents {
+    var formatted: String {
+        @Dependency(\.calendar) var calendar
+        guard let date = calendar.date(from: self) else { return "" }
+        return date.formatted(date: .omitted, time: .shortened)
+    }
+
+    var relativeClockPosition: Double {
+        let maxMinutes = 24.0 * 60.0
+        let minutes = Double(hour ?? 0) * 60 + Double(minute ?? 0)
+        return minutes/maxMinutes
+    }
+}
+
+private extension Period {
+    var clockView: some View {
+        HStack {
+            ZStack {
+                Circle()
+                    .stroke(Color.primary.opacity(25/100), lineWidth: 5)
+                    .frame(width: 40, height: 40)
+                Circle()
+                    .rotation(.radians(-.pi/2))
+                    .trim(from: start.relativeClockPosition, to: end.relativeClockPosition)
+                    .stroke(Color.green, lineWidth: 5)
+                    .frame(width: 40, height: 40)
+            }
+            Text(start.formatted).monospacedDigit()
+            Image(systemName: "arrowshape.forward")
+            Text(end.formatted).monospacedDigit()
+        }
+        .accessibilityLabel(
+            Text("\(start.formatted) to \(end.formatted)", comment: "<Hour:Minutes> to <Hour:Minutes>")
+        )
     }
 }
 
