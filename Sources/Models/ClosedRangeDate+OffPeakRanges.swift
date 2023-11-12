6081136130d22f85ebbf80099c2224a67e8ba17d
Renaud Jenny <renaud.jenny@gmail.com>
2023-11-12 15:19:11 +0100

    feat(Models): draft of a new algorithm to find the off peak ranges
    * The current one is not satisfying, and while adding tests, I realised
      it's not giving right time depending on `now`

--- Sources/Models/ClosedRangeDate+OffPeakRanges.swift
+++ Sources/Models/ClosedRangeDate+OffPeakRanges.swift
@@ -1,5 +1,35 @@
 import Foundation
 
+// TODO: dance of PeriodLegacy and rename this one to Period
+public struct PeriodMinute {
+    public let start: Int
+    public let end: Int
+
+    func range(
+        from date: Date,
+        calendar: Calendar,
+        direction: Calendar.SearchDirection = .forward
+    ) -> ClosedRange<Date>? {
+        guard 
+            let start = calendar.date(
+                bySettingHour: start/60,
+                minute: start % 60,
+                second: 0,
+                of: date,
+                direction: direction
+            ),
+            let end = calendar.date(
+                bySettingHour: end/60,
+                minute: end % 60,
+                second: 0,
+                of: date,
+                direction: direction
+            )
+        else { return nil }
+        return start...end
+    }
+}
+
 public extension [ClosedRange<Date>] {
     static func offPeakRanges(_ periods: [Period], now: Date, calendar: Calendar) -> Self {
         periods.flatMap { period -> [ClosedRange<Date>] in
@@ -25,5 +55,40 @@ public extension [ClosedRange<Date>] {
                 }
             }
         }
+    }
+
+    static func nextOffPeakRanges(_ periods: [Period], now: Date, calendar: Calendar) -> Self {
+        let currentDayMinutes = calendar.component(.hour, from: now) * 60 + calendar.component(.minute, from: now)
+        let periodsMinutes = periods.flatMap(\.periodMinutes)
+
+        return periodsMinutes.flatMap { period -> [ClosedRange<Date>] in
+            guard period.end > currentDayMinutes else { return [] }
+            var ranges: [ClosedRange<Date>] = []
+
+            if (period.start...period.end).contains(currentDayMinutes) {
+                period.range(from: now, calendar: calendar, direction: .backward).map { ranges.append($0) }
+            }
+
+            return ranges
+        }
+    }
+}
+
+extension Period {
+    var periodMinutes: [PeriodMinute] {
+        guard let startHour = start.hour, let startMinute = start.minute,
+              let endHour = end.hour, let endMinute = end.minute
+        else { return [] }
+        let startMinutes = startHour * 60 + startMinute
+        let endMinutes = endHour * 60 + endMinute
+
+        guard endMinutes > startMinutes else {
+            return [
+                PeriodMinute(start: -(60 * 24 - startMinutes) , end: endMinutes),
+                PeriodMinute(start: startMinutes, end: endMinutes + 60 * 24)
+            ]
+        }
+        
+        return [PeriodMinute(start: startMinutes, end: endMinutes)]
     }
 }
