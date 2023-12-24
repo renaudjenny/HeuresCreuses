c8206d813fe333ee9a50aa584b8ac1e0631b70b8
Renaud Jenny <renaud.jenny@gmail.com>
2023-12-24 13:52:25 +0100

    feat(OffPeak): improve ClockWidget view for Apple Watch

--- Sources/OffPeak/OffPeakSelectionView.swift
+++ Sources/OffPeak/OffPeakSelectionView.swift
@@ -128,71 +128,94 @@ public struct OffPeakSelectionView: View {
     }
 
     private func clockWidgetView(periods: [Period], minute: Double) -> some View {
+        ViewThatFits {
+            clockWidthView(periods: periods, minute: minute, scale: 80.0/100.0)
+                .padding()
+                .frame(minWidth: 200)
+
+            clockWidthView(periods: periods, minute: minute, scale: 85.0/100.0)
+        }
+    }
+
+    private func clockWidthView(periods: [Period], minute: Double, scale: Double) -> some View {
         ZStack {
             Circle()
                 .fill(Color.primary.opacity(15/100))
             Circle()
-                .scale(75/100)
+                .scale(scale)
                 .fill(backgroundColor)
 
             ForEach(periods) { period in
-                clockWidgetPeriod(period)
+                clockWidgetPeriod(period, scale: scale)
             }
 
-            clockWidgetIndicators(minute: minute)
-            clockWidgetNumbersView
+            clockWidgetIndicators(minute: minute, scale: scale)
+            clockWidgetNumbersView(scale: scale)
         }
-        .padding()
     }
 
-    private func clockWidgetPeriod(_ period: Period) -> some View {
+    private func clockWidgetPeriod(_ period: Period, scale: Double) -> some View {
         GeometryReader { geometryProxy in
+            let radius = min(geometryProxy.size.width, geometryProxy.size.height)
+            let lineWidth = radius * (1 - scale) * 40/100
+
             Circle()
                 .rotation(.radians(-.pi/2))
                 .trim(from: period.relativeClockPosition.start, to: period.relativeClockPosition.end)
-                .stroke(Color.green, style: StrokeStyle(lineWidth: geometryProxy.size.width * 10/100, lineCap: .round))
-                .padding(geometryProxy.size.width * 6.3/100)
+                .stroke(Color.green, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                .padding(lineWidth * 63/100)
         }
     }
 
-    private func clockWidgetIndicators(minute: Double) -> some View {
+    private func clockWidgetIndicators(minute: Double, scale: Double) -> some View {
         GeometryReader { geometryProxy in
-            let angle: Double = (2 * .pi)/Double(24 * 60) * Double(minute) - .pi/2
-            let radius = min(geometryProxy.size.width, geometryProxy.size.height) * 40/100
+            let angle: Double = (2.0 * .pi)/(24.0 * 60.0) * Double(minute) - .pi/2.0
+            let indicatorHeight = min(geometryProxy.size.width, geometryProxy.size.height) * (1 - scale) * 30.0/100.0
+            let radius = min(geometryProxy.size.width, geometryProxy.size.height) * scale * 54.0/100.0
+            // TODO: Not right... find why
+            let indicatorRadius = radius - (1 + 200/100 * scale)
             Rectangle()
-                .size(CGSize(width: 2, height: geometryProxy.size.height * 7/100))
+                .size(CGSize(width: 2, height: indicatorHeight))
                 .fill(Color.accentColor)
-                .position(
-                    x: geometryProxy.size.width - 1,
-                    y: geometryProxy.size.height
-                )
+                .position(x: geometryProxy.size.width, y: geometryProxy.size.height)
                 .rotationEffect(.radians(angle - .pi/2))
                 .position(
-                    x: cos(angle) * radius + geometryProxy.size.width/2,
-                    y: sin(angle) * radius + geometryProxy.size.height/2
+                    x: cos(angle) * indicatorRadius + geometryProxy.size.width/2,
+                    y: sin(angle) * indicatorRadius + geometryProxy.size.height/2
                 )
 
             ForEach(0..<96) { i in
+                let angle: Double = (2.0 * .pi)/(96.0) * Double(i) - .pi/2.0
+                let radius = 85.0/100.0 * radius
                 Rectangle()
                     .size(CGSize(width: 1, height: i % 4 == 0 ? 6 : 2))
-                    .offset(CGSize(width: geometryProxy.size.width / 2, height: geometryProxy.size.width / 6))
-                    .rotation(.degrees(360 / 96 * Double(i)))
                     .fill(Color.primary.opacity(50/100))
+                    .position(x: geometryProxy.size.width, y: geometryProxy.size.height)
+                    .rotationEffect(.radians(angle - .pi/2))
+                    .position(
+                        x: cos(angle) * radius + geometryProxy.size.width/2,
+                        y: sin(angle) * radius + geometryProxy.size.height/2
+                    )
             }
         }
     }
 
-    private var clockWidgetNumbersView: some View {
+    private func clockWidgetNumbersView(scale: Double) -> some View {
         GeometryReader { geometryProxy in
             ForEach(0..<23) { i in
                 if i % 2 == 0 {
-                    let angle: Double = (2 * .pi)/24 * Double(i) - .pi/2
-                    let radius = min(geometryProxy.size.width, geometryProxy.size.height) * 28/100
+                    let angle: Double = (2.0 * .pi)/24.0 * Double(i) - .pi/2.0
+                    let radius = min(geometryProxy.size.width, geometryProxy.size.height) * 35.0/100.0 * scale
 
                     Text("\(i)")
+                        #if os(watchOS)
+                        .font(.caption2)
+                        #else
+                        .font(.body)
+                        #endif
                         .position(
-                            x: cos(angle) * radius + geometryProxy.size.width/2,
-                            y: sin(angle) * radius + geometryProxy.size.height/2
+                            x: cos(angle) * radius + geometryProxy.size.width/2.0,
+                            y: sin(angle) * radius + geometryProxy.size.height/2.0
                         )
                         .foregroundStyle(i % 6 == 0 ? Color.primary : Color.secondary)
                 }
