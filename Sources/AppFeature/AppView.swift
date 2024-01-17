38b58fe3c0f08d367508cbf6de0345d5a3bdef5c
Renaud Jenny <renaud.jenny@gmail.com>
2024-01-17 20:15:35 +0100

    refactor(App): modernize Previews

--- Sources/AppFeature/AppView.swift
+++ Sources/AppFeature/AppView.swift
@@ -38,46 +38,40 @@ public struct AppView: View {
     }
 }
 
-#if DEBUG
-struct AppView_Previews: PreviewProvider {
-    static var previews: some View {
-        Preview(store: Store(initialState: App.State()) { App() })
-        Preview(store: Store(initialState: App.State()) {
-            App().dependency(\.date, DateGenerator { try! Date("2023-04-10T23:50:00+02:00", strategy: .iso8601) })
-        })
-        .previewDisplayName("At 23:50")
-
-        Preview(store: Store(initialState: App.State()) {
-            App().dependency(\.date, DateGenerator { try! Date("2023-04-10T00:10:00+02:00", strategy: .iso8601) })
-        })
-        .previewDisplayName("At 00:10")
-
-        Preview(store: Store(initialState: App.State()) {
-            App().dependency(\.date, DateGenerator { try! Date("2023-04-10T02:10:00+02:00", strategy: .iso8601) })
-        })
-        .previewDisplayName("At 02:10")
-
-        Preview(store: Store(initialState: App.State()) {
-            App().dependency(\.date, DateGenerator { try! Date("2023-04-10T16:00:00+02:00", strategy: .iso8601) })
-        })
-        .previewDisplayName("At 16:00")
-
-        Preview(
-            store: Store(initialState: App.State()) {
-                App().transformDependency(\.userNotifications) {
-                    $0.stream = { .example }
-                }
-            }
-        )
-        .previewDisplayName("With Notifications")
-    }
-
-    private struct Preview: View {
-        let store: StoreOf<App>
-
-        var body: some View {
-            AppView(store: store)
-        }
-    }
+#Preview {
+    AppView(store: Store(initialState: App.State()) {
+        App()
+    })
 }
-#endif
+
+#Preview("At 23:50") {
+    AppView(store: Store(initialState: App.State()) {
+        App().dependency(\.date, DateGenerator { try! Date("2023-04-10T23:50:00+02:00", strategy: .iso8601) })
+    })
+}
+
+#Preview("At 00:10") {
+    AppView(store: Store(initialState: App.State()) {
+        App().dependency(\.date, DateGenerator { try! Date("2023-04-10T00:10:00+02:00", strategy: .iso8601) })
+    })
+}
+
+#Preview("At 02:10") {
+    AppView(store: Store(initialState: App.State()) {
+        App().dependency(\.date, DateGenerator { try! Date("2023-04-10T02:10:00+02:00", strategy: .iso8601) })
+    })
+}
+
+#Preview("At 16:00") {
+    AppView(store: Store(initialState: App.State()) {
+        App().dependency(\.date, DateGenerator { try! Date("2023-04-10T16:00:00+02:00", strategy: .iso8601) })
+    })
+}
+
+#Preview("With Notifications") {
+    AppView(store: Store(initialState: App.State()) {
+        App().transformDependency(\.userNotifications) {
+            $0.stream = { .example }
+        }
+    })
+}
